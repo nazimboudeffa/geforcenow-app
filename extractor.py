9b764c8b6270574ab4ea3ce524176281476db4c5
Nazim Boudeffa <boudeffa@gmail.com>
2020-02-13 18:21:19 +0100

    If the game has been removed from Steam

--- extractor.py
+++ extractor.py
@@ -2,6 +2,8 @@ import wget
 import json
 import os.path
 
+steamCount = 0
+
 if not os.path.exists('gfnpc.json') :
     fs = wget.download(url='https://static.nvidiagrid.net/supported-public-game-list/gfnpc.json')
     with open('gfnpc.json', encoding="utf8") as f :
@@ -15,6 +17,7 @@ else :
 for game in games :
     steamUrl = game['steamUrl']
     if steamUrl != "" :
+        steamCount += 1
         gameID = steamUrl.replace("https://store.steampowered.com/app/", "")
         if not os.path.exists("json/"+gameID+".json") :
             fs = wget.download(url='https://store.steampowered.com/api/appdetails?appids='+gameID, out="json/"+gameID+".json")
@@ -26,7 +29,12 @@ for game in games :
                 steam = f.read()
                 steamGame = json.loads(steam)
         if not os.path.exists('images/'+gameID+".jpg") :
-            print("Downloading " + game['title'])
-            wget.download(url=steamGame[gameID]['data']['header_image'], out='images/'+gameID+".jpg")
+            print("Downloading " + gameID + " " + game['title'])
+            if (steamGame[gameID]['success']== True) :
+                wget.download(url=steamGame[gameID]['data']['header_image'], out='images/'+gameID+".jpg")
+            else :
+                print(gameID + " " + game['title'] + "Doesn't exist ... Please contact GForceNow on the forum")
         else :
-            print("Already have " + game['title'])
+            print("Already have "  + gameID + " " + game['title'])
+
+print("You have " + steamCount + " Steam games")
