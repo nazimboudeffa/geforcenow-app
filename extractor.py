bb2522be65018164e993f0f56e66f1500d6587cb
Nazim Boudeffa <boudeffa@gmail.com>
2020-02-13 18:00:09 +0100

    Do not download if file exists

--- extractor.py
+++ extractor.py
@@ -4,17 +4,29 @@ import os.path
 
 if not os.path.exists('gfnpc.json') :
     fs = wget.download(url='https://static.nvidiagrid.net/supported-public-game-list/gfnpc.json')
+    with open('gfnpc.json', encoding="utf8") as f :
+        games_list = f.read()
+        games = json.loads(games_list)
 else :
     with open('gfnpc.json', encoding="utf8") as f :
         games_list = f.read()
         games = json.loads(games_list)
-    for game in games :
-        steamUrl = game['steamUrl']
-        if steamUrl != "" :
-            gameID = steamUrl.replace("https://store.steampowered.com/app/", "")
+
+for game in games :
+    steamUrl = game['steamUrl']
+    if steamUrl != "" :
+        gameID = steamUrl.replace("https://store.steampowered.com/app/", "")
+        if not os.path.exists("json/"+gameID+".json") :
             fs = wget.download(url='https://store.steampowered.com/api/appdetails?appids='+gameID, out="json/"+gameID+".json")
             with open("json/"+gameID+".json", encoding="utf8") as f:
                 steam = f.read()
                 steamGame = json.loads(steam)
+        else :
+            with open("json/"+gameID+".json", encoding="utf8") as f:
+                steam = f.read()
+                steamGame = json.loads(steam)
+        if not os.path.exists('images/'+gameID+".jpg") :
             print("Downloading " + game['title'])
-            wget.download(steamGame[gameID]['data']['header_image'], 'images/'+game['title']+".jpg")
+            wget.download(url=steamGame[gameID]['data']['header_image'], out='images/'+gameID+".jpg")
+        else :
+            print("Already have " + game['title'])
