4e7948b1f7494fb9b5ba7bb45fc436bc864bf71f
Nazim Boudeffa <boudeffa@gmail.com>
2020-02-14 03:55:08 +0100

    Added express app to display the images

--- grabber.py
+++ grabber.py
@@ -4,13 +4,13 @@ import os.path
 
 steamCount = 0
 
-if not os.path.exists('gfnpc.json') :
-    fs = wget.download(url='https://static.nvidiagrid.net/supported-public-game-list/gfnpc.json')
-    with open('gfnpc.json', encoding="utf8") as f :
+if not os.path.exists('public/gfnpc.json') :
+    fs = wget.download(url='https://static.nvidiagrid.net/supported-public-game-list/gfnpc.json', out='public/gfnpc.json')
+    with open('public/gfnpc.json', encoding="utf8") as f :
         games_list = f.read()
         games = json.loads(games_list)
 else :
-    with open('gfnpc.json', encoding="utf8") as f :
+    with open('public/gfnpc.json', encoding="utf8") as f :
         games_list = f.read()
         games = json.loads(games_list)
 
@@ -19,19 +19,19 @@ for game in games :
     if steamUrl != "" :
         steamCount += 1
         gameID = steamUrl.replace("https://store.steampowered.com/app/", "")
-        if not os.path.exists("json/"+gameID+".json") :
-            fs = wget.download(url='https://store.steampowered.com/api/appdetails?appids='+gameID, out="json/"+gameID+".json")
-            with open("json/"+gameID+".json", encoding="utf8") as f:
+        if not os.path.exists("public/json/"+gameID+".json") :
+            fs = wget.download(url='https://store.steampowered.com/api/appdetails?appids='+gameID, out="public/json/"+gameID+".json")
+            with open("public/json/"+gameID+".json", encoding="utf8") as f:
                 steam = f.read()
                 steamGame = json.loads(steam)
         else :
-            with open("json/"+gameID+".json", encoding="utf8") as f:
+            with open("public/json/"+gameID+".json", encoding="utf8") as f:
                 steam = f.read()
                 steamGame = json.loads(steam)
-        if not os.path.exists('images/'+gameID+".jpg") :
+        if not os.path.exists('public/images/'+gameID+".jpg") :
             print("Downloading " + gameID + " " + game['title'])
             if (steamGame[gameID]['success']== True) :
-                wget.download(url=steamGame[gameID]['data']['header_image'], out='images/'+gameID+".jpg")
+                wget.download(url=steamGame[gameID]['data']['header_image'], out='public/images/'+gameID+".jpg")
             else :
                 print(gameID + " " + game['title'] + "Doesn't exist ... Please contact GForceNow on the forum")
         else :
