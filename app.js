c864c606014160d24b280a5fc2cdf29c45dc4026
Nazim Boudeffa <boudeffa@gmail.com>
2020-03-13 02:30:38 +0100

    Added no image preview

--- app.js
+++ app.js
@@ -3,15 +3,32 @@ const express = require('express')
 const app = express()
 const ejs = require('ejs')
 const path = require('path')
+var gfn = require('./public/data/gfnpc.json')
+var noImages = [];
 
 // set the view engine to ejs
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, '/public'))
 
+gfn.forEach((item, i) => {
+  if (item.steamUrl != ""){
+    str = item.steamUrl
+    val = str.replace("https://store.steampowered.com/app/","")
+    if (!fs.existsSync("./public/images/"+val+".jpg")) {
+      noImages.push(val)
+    }
+  }
+});
+
+
+console.log(noImages.length);
+console.log(noImages);
+
+
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-  res.render('index')
+  res.render('index', {noImages:noImages})
 })
 
 app.get('/all', function (req, res) {
